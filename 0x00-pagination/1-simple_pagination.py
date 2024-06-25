156db5b52bef3ab240e317c533cb6d935b3872b8
xilder <olabowaleayobami@gmail.com>
2024-06-25 16:11:20 +0100

    updates task 1: pass checker 5 - fix 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -41,10 +41,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) == int
-        assert type(page_size) == int
-        assert page > 0
-        assert page_size > 0
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
         start, end = index_range(page, page_size)
         try:
             return self.dataset()[start: end]
