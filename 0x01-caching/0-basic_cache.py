d31a3e7df763be3140becd91e9b6b277e4954fa1
xilder <olabowaleayobami@gmail.com>
2024-06-26 22:20:42 +0100

    adds task 2

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -21,10 +21,12 @@ class BasicCache(BaseCaching):
         """
         put method
         """
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
         get method
         """
-        return self.cache_data.get(key, None)
+        if key:
+            return self.cache_data.get(key, None)
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+FIFO cache class
+"""
+from collections import deque
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache class
+    """
+    def __init__(self):
+        """init"""
+        super().__init__()
+        self.queue = deque()
+
+
+    def put(self, key, item):
+        """put method"""
+        if key and item:
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+        if len(self.cache_data.keys()) > self.MAX_ITEMS:
+            k = self.queue.popleft()
+            del self.cache_data[k]
+
+
+    def get(self, key):
+        """get method"""
+        if key:
+            return self.cache_data.get(key, None)
