5acecdd250a2efda6d5053b012a910a0d9b6f4dd
xilder <olabowaleayobami@gmail.com>
2024-07-02 22:15:58 +0100

    adds task 2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Babel configuration for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
+babel = Babel(app)
 
 
 @app.route("/")
