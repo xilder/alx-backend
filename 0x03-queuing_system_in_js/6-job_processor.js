1550b3cc31559f0bbae5fff9ef769effbfaaca04
xilder <olabowaleayobami@gmail.com>
2024-07-31 22:49:10 +0100

    adds task 7

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,10 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue({ name: 'push_notification_code' });
+const sendNotification = (phoneNumber, message) =>
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
